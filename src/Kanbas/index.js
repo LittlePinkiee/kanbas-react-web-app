eabc38c5acb74299a137b97a6fcff0f7b13751f7
Pingting Liu <liu.ping@northeastern.edu>
2023-10-19 14:30:32 -0700

    update A3 Kanbas Nav

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,4 +1,5 @@
 import { Route, Routes, Navigate } from "react-router";
+import Nav from "../Nav";
 import KanbasNavigation from "./KanbasNavigation";
 import Courses from "./Courses";
 import Account from "./Account";
@@ -6,20 +7,24 @@ import Dashboard from "./Dashboard";
 
 function Kanbas() {
   return (
-    <div className="d-flex">
-      <div>
-        <KanbasNavigation />
+    <>
+      <Nav />
+      <div className="d-flex">
+        <div>
+          <KanbasNavigation />
+        </div>
+        <div className="w-full">
+          <Routes>
+            <Route path="/" element={<Navigate to="Dashboard" />} />
+            <Route path="Account" element={<Account />} />
+            <Route path="Dashboard" element={<Dashboard />} />
+            <Route path="Courses/:courseId/*" element={<Courses />} />
+            <Route path="Calendar" element={<h1>Calendar</h1>} />
+          </Routes>
+        </div>
       </div>
-      <div className="w-full">
-        <Routes>
-          <Route path="/" element={<Navigate to="Dashboard" />} />
-          <Route path="Account" element={<Account />} />
-          <Route path="Dashboard" element={<Dashboard />} />
-          <Route path="Courses/:courseId/*" element={<Courses />} />
-          <Route path="Calendar" element={<h1>Calendar</h1>} />
-        </Routes>
-      </div>
-    </div>
+    </>
+
   );
 }
 export default Kanbas;
