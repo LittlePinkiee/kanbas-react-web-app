74dedac7516ff0a7fcb10e03419e6fa51d164831
Pingting Liu <liu.ping@northeastern.edu>
2023-10-26 20:44:59 -0700

    A4 lab

---
+++ src/Labs/a4/ReduxExamples/todos/TodoList.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import TodoItem from "./TodoItem";
+import { useSelector } from "react-redux";
+
+function TodoList() {
+  const { todos } = useSelector((state) => state.todosReducer);
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <ul className="list-group">
+        <TodoForm />
+        {todos.map((todo) => (
+          <TodoItem todo={todo} />
+        ))}
+
+        {/* <li className="list-group-item">
+          <button onClick={() => addTodo(todo)}>Add</button>
+          <button onClick={() => updateTodo(todo)}>
+            Update </button>
+          <input
+            value={todo.title}
+            onChange={(e) =>
+              setTodo({
+                ...todo,
+                title: e.target.value,
+              })
+            }
+          />
+        </li>
+        {todos.map((todo) => (
+          <li key={todo.id} className="list-group-item">
+            <button onClick={() => deleteTodo(todo.id)}>
+              Delete </button>
+            <button onClick={() => setTodo(todo)}>
+              Edit </button>
+            {todo.title}
+          </li>
+        ))} */}
+      </ul>
+    </div>
+  );
+}
+export default TodoList;
--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,19 +1,23 @@
-import { Link } from "react-router-dom";
-import Assignment3 from "./a3";
 import Nav from "../Nav";
-
+import Assignment3 from "./a3";
+import Assignment4 from "./a4";
+import { Routes, Route, Navigate } from "react-router";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function Labs() {
-   return (
+  return (
+    <Provider store={store}>
       <div>
-         {/* <h1>Assignment 3</h1> */}
-         <Nav/>
-{/* 
-         <Link to="/hello">Hello</Link> |
-         <Link to="/Labs/a3">A3</Link> |
-         <Link to="/Kanbas">Kanbas</Link> */}
-         <Assignment3 />
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Navigate to="a3" />} />
+          <Route path="a3" element={<Assignment3 />} />
+          <Route path="a4" element={<Assignment4 />} />
+        </Routes>
       </div>
-   );
+    </Provider>
+  );
 }
+
 export default Labs;
